d7f08379f4f36bf2a31b1f4ace8e0907511886cb
Jason Kobuchi <jekobuchi@gmail.com>
2023-02-27 17:18:04 -0800

    added initial state and imported Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+import "./App.css";
 
 function App() {
+  const [ProductsData, setProductsData] = useState<
+    {
+      id: string;
+      title: string;
+      price: number;
+      description: string;
+      category: string;
+      image: string;
+      rating: { rate: number; count: number };
+    }[]
+  >([]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
       </header>
+      <main></main>
     </div>
   );
 }

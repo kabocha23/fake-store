683b341b9da9fed3a89c8a0fdb59e6218d6140d8
Jason Kobuchi <jekobuchi@gmail.com>
2023-02-27 17:19:09 -0800

    created Navbar component

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar-container">
+      <header>This is the Navbar header</header>
+      <main></main>
+    </div>
+  );
+};
+
+export default Navbar;

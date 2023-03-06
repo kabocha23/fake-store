7eb3ca3c5866a87e8c67e3b0a810ab204d8c9e47
Jason Kobuchi <jekobuchi@gmail.com>
2023-03-06 15:59:37 -0800

    added logic for category filter on products, added prop types to interface, select tag with options for categories, and filter on mapped products

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -12,23 +12,55 @@ interface ProductsProps {
     image: string;
     rating: { rate: number; count: number };
   }[];
+  categoryFilter: string;
+  setCategoryFilter: React.Dispatch<React.SetStateAction<string>>;
+  handleSelect: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 }
 
-const Products: FC<ProductsProps> = ({ productsData }) => {
+const Products: FC<ProductsProps> = ({
+  productsData,
+  categoryFilter,
+  setCategoryFilter,
+  handleSelect,
+}) => {
   return (
     <div className="products-container">
-      {productsData.map((productData) => (
-        <Product
-          key={`key-${productData.id}`}
-          prodId={productData.id}
-          title={productData.title}
-          price={productData.price}
-          description={productData.description}
-          category={productData.category}
-          image={productData.image}
-          rating={productData.rating}
-        />
-      ))}
+      <div className="products-filter">
+        <label htmlFor="categorySelect">Filter by: </label>
+
+        <select
+          name="categorySelect"
+          defaultValue="Select Category"
+          id="categorySelector"
+          onChange={(e) => handleSelect(e)}
+        >
+          <option value="Select Category">Select Category</option>
+          <option value="men's clothing">Men's Clothing</option>
+          <option value="women's clothing">Women's Clothing</option>
+          <option value="electronics">Electronics</option>
+          <option value="jewelery">Jewelry</option>
+        </select>
+      </div>
+      <div className="products-list">
+        {productsData
+          .filter((product) =>
+            categoryFilter === "Select Category"
+              ? product.category.length > 0
+              : product.category === categoryFilter
+          )
+          .map((productData) => (
+            <Product
+              key={`key-${productData.id}`}
+              prodId={productData.id}
+              title={productData.title}
+              price={productData.price}
+              description={productData.description}
+              category={productData.category}
+              image={productData.image}
+              rating={productData.rating}
+            />
+          ))}
+      </div>
     </div>
   );
 };

3e50e53b98db6a4860b6a413cc1d3477caed7e37
Jason Kobuchi <jekobuchi@gmail.com>
2023-03-27 16:39:55 -0700

    created AddToCart component to be added to ProductPage

---
+++ src/components/AddToCart/AddToCart.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import "./AddToCart.css";
+
+interface ProductProps {
+  quantity: number;
+  decrementQty: React.MouseEventHandler<HTMLButtonElement>;
+  incrementQty: React.MouseEventHandler<HTMLButtonElement>;
+  onAddToCart: (itemId: number, quantity: number) => void;
+  cartIcon: string;
+  idNum: number;
+}
+
+const AddToCart: FC<ProductProps> = ({
+  quantity,
+  decrementQty,
+  incrementQty,
+  onAddToCart,
+  cartIcon,
+  idNum,
+}) => {
+  return (
+    <div className="productpage-addtocart">
+      <div className="addToCart-counter">
+        <button onClick={decrementQty} disabled={quantity === 0}>
+          <span className="addToCart-counter-change">-</span>
+        </button>
+        <span className="addToCart-count">{quantity}</span>
+        <button onClick={incrementQty}>
+          <span className="addToCart-counter-change">+</span>
+        </button>
+      </div>
+      <div className="addToCart-submit">
+        <button
+          onClick={() => onAddToCart(idNum, quantity)}
+          disabled={quantity === 0}
+        >
+          <img src={cartIcon} alt="cart"></img>
+          <span>Add to Cart</span>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default AddToCart;

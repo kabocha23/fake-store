5977d92e921148aa11b1192e203d78db2790e1b4
Jason Kobuchi <jekobuchi@gmail.com>
2023-03-27 16:47:00 -0700

    added interface for cartIcon and added as img tag

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,19 @@
 import { FC } from "react";
 import "./Navbar.css";
 
-const Navbar: FC = () => {
+interface NavbarProps {
+  cartIcon: string;
+}
+
+const Navbar: FC<NavbarProps> = ({ cartIcon }) => {
   return (
     <div className="navbar-container">
       <p>
         <a href="">Sign in</a> or <a href="">register</a>
       </p>
-      <a>cart placeholder</a>
+      <div>
+        <img src={cartIcon} alt="cart"></img>
+      </div>
     </div>
   );
 };

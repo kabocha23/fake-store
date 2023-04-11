530fb55d10d1c4de1ee5879124bb796dc465cb9f
Jason Kobuchi <jekobuchi@gmail.com>
2023-04-11 12:31:08 -0700

    added quantity to dependency array to update initial cart add and make pop up appear. Implemented CartModal popup onclick of cart icon, passed appropriate properties to Navbar

--- src/components/CartModal/CartModal.tsx
+++ src/components/CartModal/CartModal.tsx
@@ -1,4 +1,5 @@
 import { FC } from "react";
+import deleteIcon from "../../images/icon-delete.svg";
 import "./CartModal.css";
 
 interface CartModalProps {
@@ -11,11 +12,55 @@ interface CartModalProps {
     image: string;
     rating: { rate: number; count: number };
   }[];
-  cart: { [key: number]: number }[];
+  cart: { productId: number; productQty: number }[];
+  cartTotal: number;
+  setCartTotal: React.Dispatch<React.SetStateAction<number>>;
+  onRemoveFromCart: () => void;
 }
 
-const CartModal: FC<CartModalProps> = ({ productsData, cart }) => {
-  return <div className="cartmodal-container"></div>;
+const CartModal: FC<CartModalProps> = ({
+  productsData,
+  cart,
+  cartTotal,
+  onRemoveFromCart,
+}) => {
+  return (
+    <div className="cart-modal-container">
+      <div className="cart-title">
+        <p>Cart</p>
+      </div>
+      <div className="cart-main">
+        {cart.length === 0 ? (
+          <p id="empty-cart-msg">Your cart is empty</p>
+        ) : (
+          <div className="cart-has-products">
+            <div className="cart-products">
+              {cart.map((product) => {
+                return (
+                  <div className="cart-each-product">
+                    <p key={product.productId} id="cart-product-name">
+                      {productsData[product.productId].title}
+                    </p>
+                    <p id="cart-product-qty"> x{product.productQty}</p>
+                    <div id="cart-product-delete">
+                      <img
+                        src={deleteIcon}
+                        onClick={() => onRemoveFromCart}
+                        alt="delete"
+                      ></img>
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="cart-checkout">
+              <button>Checkout</button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default CartModal;

b2ae1af83bfe4f4d6dfbcc55d18e9c5cfd4c7ce0
Jason Kobuchi <jekobuchi@gmail.com>
2023-07-18 15:03:52 -0700

    changed tile to route to product page only when clicking the image or title, previously clicking anywhere on tile routed

--- src/components/ProductTile/ProductTile.tsx
+++ src/components/ProductTile/ProductTile.tsx
@@ -19,16 +19,18 @@ const ProductTile: FC<ProductProps> = ({
   rating,
 }) => {
   return (
-    <Link to={`/product/${prodId}`}>
-      <div className="producttile-container">
+    <div className="producttile-container">
+      <Link to={`/product/${prodId}`}>
         <div className="producttile-image">
           <img src={image} alt={title}></img>
         </div>
-        <h1 id="producttile-title">{title}</h1>{" "}
-        <h2 id="producttile-rating">{`${rating.rate} stars ${rating.count} reviews`}</h2>
-        <h2 id="producttile-price">${price.toFixed(2)}</h2>
-      </div>
-    </Link>
+      </Link>
+      <Link to={`/product/${prodId}`}>
+        <h1 id="producttile-title">{title}</h1>
+      </Link>
+      <h2 id="producttile-rating">{`${rating.rate} stars ${rating.count} reviews`}</h2>
+      <h2 id="producttile-price">${price.toFixed(2)}</h2>
+    </div>
   );
 };
 
